b420dba2e8d59d35ed1fc6d7590137d946b19c82
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2024-06-18 23:50:11 +0200

    feat: Sort hunts by default

--- src/controllers/hunts.controller.ts
+++ src/controllers/hunts.controller.ts
@@ -1,6 +1,6 @@
 import { Hunt } from "../models/hunt.model";
 
 export async function getHunts(req, res) {
-    const hunts = await Hunt.find();
+    const hunts = await Hunt.find().sort({ updatedAt: -1 });
     return res.status(200).json(hunts);
 }
