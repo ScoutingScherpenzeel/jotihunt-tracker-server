321ba47ddeb2a29bbc0d7086d697d6e358b333e3
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2024-10-18 19:13:20 +0200

    feat: Routes for updating the area of a team

--- src/controllers/teams.controller.ts
+++ src/controllers/teams.controller.ts
@@ -5,3 +5,13 @@ export async function getTeams(_req: Request, res: Response) {
   const teams = await Team.find();
   return res.status(200).json(teams);
 }
+
+export async function setTeamArea(req: Request, res: Response) {
+  const { id } = req.params;
+  const { area } = req.body;
+
+  const team = await Team.findById(id);
+  team!.area = area;
+  await team!.save();
+  res.status(200).json(team);
+}
--- src/routes/teams.route.ts
+++ src/routes/teams.route.ts
@@ -1,9 +1,37 @@
 import express from "express";
 import * as teamsController from "../controllers/teams.controller";
 import verifyToken from "../middleware/auth.middleware";
+import { body, param } from "express-validator";
+import { Area } from "../models/area.model";
+import { validate } from "../middleware/validate.middleware";
+import { Team } from "../models/team.model";
 
+// Validations
+const updateAreaValidator = [
+  body("area").notEmpty().withMessage("Area must not be empty"),
+  body("area").isString().withMessage("Area must be a string"),
+  body("area").isMongoId().withMessage("Area must be a mongoId"),
+  body("area").custom(async (value) => {
+    const area = await Area.findById(value);
+    if (!area) {
+      throw new Error("Area must exist in the areas table");
+    }
+
+    return true;
+  }),
+  param("id").notEmpty().withMessage("Id must not be empty"),
+  param("id").isMongoId().withMessage("Id must be a mongoId"),
+  param("id").custom(async (value) => {
+    const team = await Team.findById(value);
+    if (!team) {
+      throw new Error("Team must exist in the teams table");
+    }
+    return true;
+  }),
+];
 const router = express.Router();
 
 router.get("/", verifyToken, teamsController.getTeams);
+router.put("/:id/area", [verifyToken, validate(updateAreaValidator)], teamsController.setTeamArea);
 
 export default router;
